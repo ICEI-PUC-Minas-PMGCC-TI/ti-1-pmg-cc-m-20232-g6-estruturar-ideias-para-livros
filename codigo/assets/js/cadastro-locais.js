18ee52bc9d10d5fd93a1d02870b1ae9783632da7
Luiz Felipe <luizfelipeespf@gmail.com>
2023-10-22 22:19:38 -0300

    json locais, js locais, alteração no html locais-2

--- codigo/assets/js/cadastro-locais.js
+++ codigo/assets/js/cadastro-locais.js
@@ -1,62 +1,99 @@
 const databaseLink = "https://json-server-luizfelipe-cadastrolocais.luizfelipe353.repl.co/locais";
-    function carregarLocais() {
-        fetch('https://json-server-luizfelipe-cadastrolocais.luizfelipe353.repl.co/locais')
-            .then(response => response.json())
-            .then(data => {
-                const listaLocais = document.querySelector('.list-group'); // Selecione a lista de locais
+let locaisParaExcluir = []; // Array para armazenar os locais marcados para exclusão
 
-                // Limpe a lista atual
-                listaLocais.innerHTML = '';
+function carregarLocais() {
+    fetch(databaseLink)
+        .then(response => response.json())
+        .then(data => {
+            const listaLocais = document.querySelector('.list-group'); // Selecione a lista de locais
 
-                // Adicione os locais do servidor à lista
-                data.forEach(local => {
-                    const localItem = document.createElement('a');
-                    localItem.className = 'list-group-item list-group-item-action';
-                    localItem.textContent = local.nome;
-                    listaLocais.appendChild(localItem);
-                });
-            })
-            .catch(error => {
-                console.error('Erro ao carregar locais:', error);
+            // Limpe a lista atual
+            listaLocais.innerHTML = '';
+
+            // Adicione os locais do servidor à lista
+            data.forEach(local => {
+                const localItem = document.createElement('a');
+                localItem.className = 'list-group-item list-group-item-action';
+                localItem.textContent = local.nome;
+
+                // Adicione um evento de clique para marcar o local para exclusão
+                localItem.addEventListener('click', () => toggleExcluirLocal(local));
+
+                listaLocais.appendChild(localItem);
             });
-    }
-
-    // Função para salvar um novo local
-    function salvarLocal() {
-        const nome = document.getElementById('inputNomedoLocal').value;
-        const arquitetura = document.getElementById('inputArquiteturadoLocal').value;
-        const geografia = document.getElementById('inputGeografiadoLocal').value;
-        const historia = document.getElementById('inputHistoriadoLocal').value;
-
-        const novoLocal = {
-            nome,
-            'arquitetura do local': arquitetura,
-            'geografia do local': geografia,
-            'história do local': historia
-        };
-
-        fetch('https://json-server-luizfelipe-cadastrolocais.luizfelipe353.repl.co/locais', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-            },
-            body: JSON.stringify(novoLocal),
         })
-            .then(response => response.json())
-            .then(data => {
-                console.log('Local criado:', data);
-                carregarLocais(); // Atualize a lista de locais após criar um novo local
+        .catch(error => {
+            console.error('Erro ao carregar locais:', error);
+        });
+}
+
+// Função para salvar um novo local
+function salvarLocal(novoLocal) {
+    fetch(databaseLink, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(novoLocal),
+    })
+        .then(response => response.json())
+        .then(data => {
+            console.log('Novo local criado:', data);
+            carregarLocais(); // Atualize a lista de locais após criar um novo local
+        })
+        .catch(error => {
+            console.error('Erro ao criar local:', error);
+        });
+}
+
+// Evento de clique no botão "Salvar"
+const botaoSalvar = document.querySelector('.btn-success');
+botaoSalvar.addEventListener('click', () => {
+    const nome = document.getElementById('inputNomedoLocal').value;
+    const arquitetura = document.getElementById('inputArquiteturadoLocal').value;
+    const geografia = document.getElementById('inputGeografiadoLocal').value;
+    const historia = document.getElementById('inputHistoriadoLocal').value;
+
+    const novoLocal = {
+        nome,
+        'arquitetura do local': arquitetura,
+        'geografia do local': geografia,
+        'história do local': historia
+    };
+
+    salvarLocal(novoLocal); // Chame a função salvarLocal ao salvar um novo local
+});
+
+// Função para marcar/desmarcar um local para exclusão
+function toggleExcluirLocal(local) {
+    const localIndex = locaisParaExcluir.findIndex(item => item.id === local.id);
+    if (localIndex === -1) {
+        locaisParaExcluir.push(local);
+    } else {
+        locaisParaExcluir.splice(localIndex, 1);
+    }
+}
+
+// Função para excluir locais marcados para exclusão
+function excluirLocaisMarcados() {
+    locaisParaExcluir.forEach(local => {
+        fetch(`${databaseLink}/${local.id}`, {
+            method: 'DELETE',
+        })
+            .then(() => {
+                console.log(`Local com ID ${local.id} excluído.`);
             })
             .catch(error => {
-                console.error('Erro ao criar local:', error);
+                console.error('Erro ao excluir local:', error);
             });
-    }
+    });
 
-    // Função para excluir um local
-    function excluirLocal() {
-        // Aqui você pode implementar a lógica para excluir um local
-        // Certifique-se de chamar carregarLocais() após a exclusão para atualizar a lista
-    }
+    // Limpe a lista de locais marcados para exclusão
+    locaisParaExcluir = [];
 
-    // Chame carregarLocais() para exibir os locais quando a página carregar
+    // Atualize a lista de locais
     carregarLocais();
+}
+
+// Chame carregarLocais() para exibir os locais quando a página carregar
+carregarLocais();
